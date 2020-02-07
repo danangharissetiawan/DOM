d5f322a3b7b867b568c3141e30de5c31d843b016
danangharissetiawan <Mr.danangharissetiawan@gmail.com>
2020-02-08 01:46:35 +0700

    Eps 00 Pendahuluan dan pengenalan DOM

---
+++ 00_Pendahuluan_&_Pengenalan/Main.js
@@ -0,0 +1,21 @@
+/*
+# Definisi ----
+DOM = - Antarmuka pemrograman untuk HTML yang merepresentasikan halaman web.
+        Sehingga program dapat merubah dan memanipulasi nya.
+      - Representasi element HTML pada document menjadi sebuah object.
+
+    * Document = Keranga atau isi website.
+    * Object = Sebuah nilai atau tipe data dalam java scripts.
+    * Model = Representasi terhadap sesuatu.
+
+# Mengenal DOM -----
+- Antar muka permrograman berbass object yang merepresentasikan dokument web.
+- Sehingga membuat seluruh komponen dari halaman web bisa  diakses dan di mainipulasi
+- DOM dapat dimanipulasi menggunakan Javascripts.
+
+# Materi Yang Akan di pelajari.
+- DOM Selection         - Event Heandling
+- DOM Manipulation      - Studi Kasus
+- DOM Traversal
+    
+*/
